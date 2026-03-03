1aab411143987dac8f218ee1096499705db6769d
Léa <lea.crenn.durif@gmail.com>
2026-03-03 09:58:23 +0100

    [FIX] Import

--- API_Users/src/main/java/com/repositories/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.com.repositories;
-
-import src.main.java.com.entities.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
