2d0dccd9ab003e4bd664c4398cf93c583970dbea
Timothée Benchergui <timothee.benchergui@gmail.com>
2026-03-03 08:19:42 +0100

    Add AppHeader component and update main template structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,12 @@
-<script setup></script>
+<script setup>
+import AppHeader from '@/components/AppHeader.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+    <AppHeader />
+    <RouterView />
+  </main>
 </template>
 
 <style scoped></style>
